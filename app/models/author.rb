6009180f2accc63832945eee03bba90a24ed7d56
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-30 19:08:29 +0300

    feat(Article): Filter the author of an article

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,12 +1,13 @@
-require 'pry'
-
 class Author
   attr_reader :name
 
 
   def initialize(name)
-    @name = name
-  
+    @name = name 
+  end
+  def articles
+    Article.all.select{|article|
+    article.author==@name}
   end
 
 
